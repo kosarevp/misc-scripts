538c78580189563fa42541bdd2acf918a026c939
kosarevp <34346736+kosarevp@users.noreply.github.com>
2022-04-11 14:03:52 +0400

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -1,3 +1,3 @@
 sudo apt update && sudo apt upgrade -y && sudo apt install wireguard -y
-[ ! -d /etc/wireguard ] && sudo mkdir /etc/wireguard;
+[ ! -d /etc/wireguard ] && sudo mkdir /etc/wireguard
 cd /etc/wireguard; sudo umask 077; sudo wg genkey | sudo tee wg0-private.key | sudo wg genpub > wg0-public.key
