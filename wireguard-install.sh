c481f67e54d1b70bfac913ad530e19e0ebde0e80
kosarevp <34346736+kosarevp@users.noreply.github.com>
2022-04-11 14:42:56 +0400

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -3,6 +3,6 @@
 sudo su
 apt update && apt upgrade -y && apt install wireguard -y
 [ ! -d /etc/wireguard ] && mkdir /etc/wireguard
-cd /etc/wireguard; umask 077; wg genkey | tee wg0-private.key | wg genpub > wg0-public.key
+cd /etc/wireguard; umask 077; wg genkey | tee wg0-private.key | wg pubkey > wg0-public.key
 mkdir /etc/wireguard/clients && cd "$_"
-for N in {1..10}; do umask 077; wg genkey | tee wg0-client$N-private.key | wg genpub > wg0-client$N-public.key && wg genpsk > wg0-client$N-preshared.key; done
+for N in {1..10}; do umask 077; wg genkey | tee wg0-client$N-private.key | wg pubkey > wg0-client$N-public.key && wg genpsk > wg0-client$N-preshared.key; done
